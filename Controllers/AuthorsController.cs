b16f0e2aee86051da37fb927a3209a11901f9868
yonnybeyene <yonnybeyene@github.com>
2020-05-05 13:17:22 -0400

    commit

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -1,8 +1,9 @@
+using Fisher.Bookstore.Models;
+using Fisher.Bookstore.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Fisher.Bookstore.Controllers
 {
 
-    public class AuthorsController
-    {
 
     }
-}
--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,10 @@ namespace Fisher.Bookstore
                 options.UseNpgsql(Configuration.GetConnectionString("BookstoreContext"))
             );
             services.AddScoped<IBooksRepository, BooksRepository>();
+            services.AddSingleton<IBooksRepository, TestBooksRepository>();
+
+        //    services.AddScoped<IAuthorsRepository, AuthorsRepository>();
+            services.AddSingleton<IAuthorsRepository, TestAuthorsRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
