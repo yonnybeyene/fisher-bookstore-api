f656606a07eb0fa85602274907603e3d8f62decd
yonnybeyene <yonnybeyene@github.com>
2020-05-06 18:30:00 -0400

    commit

--- Models/Book.cs
+++ Models/Book.cs
@@ -7,5 +7,11 @@ namespace Fisher.Bookstore.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public List<BookAuthor> BookAuthor { get; set; }
+        public double Price { get; set; }
+
+        public void ApplyMemberDiscount()
+        {
+            Price = Price * .90;
+        }
     }
 }
