c677eceeb1be0d483349b93019aafe61015ebee2
yonnybeyene <yonnybeyene@github.com>
2020-05-05 22:15:32 -0400

    commit

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -4,6 +4,70 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Fisher.Bookstore.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthorsController : ControllerBase
+    {
 
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(authorsRepository.GetAuthors());
+        }
+
+        private IAuthorsRepository authorsRepository;
+
+        public AuthorsController(IAuthorsRepository repository)
+        {
+            authorsRepository = repository;
+        }
+
+        [HttpGet("{authorId")]
+        public IActionResult Get(int authorId)
+        {
+            if (!authorsRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            return Ok(authorsRepository.GetAuthor(authorId));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Author author)
+        {
+            var authorId = authorsRepository.AddAuthor(author);
+            return Created($"https://localhost:5001/api/authors/{authorId}", author);
+        }
+
+        [HttpPut("{authorId}")]
+        public IActionResult Put(int authorId, [FromBody] Author author)
+        {
+            if (authorId != author.Id)
+            {
+                return BadRequest();
+            }
+
+            if (!authorsRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            authorsRepository.UpdateAuthor(author);
+            return Ok(author);
+        }
+
+        [HttpDelete("{authorId}")]
+        public IActionResult Delete(int authorId)
+        {
+            if (!authorsRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            authorsRepository.DeleteAuthor(authorId);
+            return Ok();
+        }
+    }
 
     }
